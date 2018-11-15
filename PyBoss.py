cb75fffee19c2c8031c21f72df37cae4d6bca152
sameralsabb <sameralsabb@gmail.com>
2018-11-14 16:56:46 -0800

    PyBoss.py added

---
+++ PyBoss.py
@@ -0,0 +1,70 @@
+import csv
+import os
+
+#Files to load and output (Remember to hange these)
+fite_to_load = os.path.join("raw_data", "employee_data1.csv")
+file_to_output = os.path.join("analysis", "employee_data_reformatted1.csv")
+
+#Dictionary of states with abbreviations
+us_state_abbrev = {
+    "Alabama": "AL",
+    "Alaska": "AK",
+    "Arizona": "AZ",
+    "Arkansas": "AR",
+    "California": "CA",
+    "Colorado": "CO",
+    "Connecticut": "CT",
+    "Delaware": "DE",
+    "Florida": "FL",
+    "Georgia": "GA",
+    "Hawaii": "HI",
+    "Idaho": "ID",
+    "Illinois": "IL",
+    "Indiana": "IN",
+    "Iowa": "IA",
+    "Kansas": "KS",
+    "Kentucky": "KY",
+    "Louisiana": "LA",
+    "Maine": "ME",
+    "Maryland": "MD",
+    "Massachusetts": "MA",
+    "Michigan": "MI",
+    "Minnesota": "MN",
+    "Mississippi": "MS",
+    "Missouri": "MO",
+    "Montana": "MT",
+    "Nebraska": "NE",
+    "Nevada": "NV",
+    "New Hampshire": "NH",
+    "New Jersey": "NJ",
+    "New Mexico": "NM",
+    "New York": "NY",
+    "North Carolina": "NC",
+    "North Dakota": "ND",
+    "Ohio": "OH",
+    "Oklahoma": "OK",
+    "Oregon": "OR",
+    "Pennsylvania": "PA",
+    "Rhode Island": "RI",
+    "South Carolina": "SC",
+    "South Dakota": "SD",
+    "Tennessee": "TN",
+    "Texas": "TX",
+    "Utah": "UT",
+    "Vermont": "VT",
+    "Virginia": "VA",
+    "Washington": "WA",
+    "West Virginia": "WV",
+    "Wisconsin": "WI",
+    "Wyoming": "WY",
+}
+
+# Placeholders for re-formatted contents
+emp_ids = []
+emp_first_names = []
+emp_last_names = []
+emp_dobs = []
+emp_ssns = []
+emp_states = []
+
+# Read the csv and convert it into a list of dictionaries
