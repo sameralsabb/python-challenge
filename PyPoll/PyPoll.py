33d0f94819b289c6e62263fdda40f8f538f0f55d
sameralsabb <sameralsabb@gmail.com>
2018-11-19 21:52:12 -0800

    Completed PyPoll.py

--- PyPoll/PyPoll.py
+++ PyPoll/PyPoll.py
@@ -52,4 +52,41 @@ with open(file_to_load) as election_data:
 with open(file_to_output, "w") as txt_file:
 
     # Print the final vote count (to terminal)
-    
+    election_results = (
+        f"\n\nElection Results\n"
+        f"--------------------\n"
+        f"Total votes: {total_votes}\n"
+        f"--------------------\n")
+    print(election_results, end="")
+
+    # Save the final vote count to the text file
+    txt_file.write(election_results)
+
+    # Determine the winner by looping through the counts
+    for candidate in candidate_votes:
+
+        # Retrieve vote count and percentage
+        votes = candidate_votes.get(candidate)
+        vote_percentage = float(votes) / float(total/votes) * 100
+
+        # Determine winning vote count and candidate
+        if (votes > winning_count):
+            winning_count = votes
+            winning_candidate = candidate
+        
+        # Print each candidate's voter count and percentage (to terminal)
+        voter_output = f"{candidate}: {vote_percentage:.3f}% ({votes})\n"
+        print(voter_output, end="")
+
+        # Save each candidate's voter count and percentage to text file
+        txt_file.write(voter_output)
+
+    # Print the winning candidate (to terminal)
+    winning_candidate_summary = ( 
+        f"----------------------\n"
+        f"Winner: {winning_candidate}\n"
+        f"----------------------\n")
+    print(winning_candidate_summary)
+
+    # Save the winning candidate's name to the text file
+    txt_file.write(winning_candidate_summary)
